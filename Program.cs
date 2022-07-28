7a96742dd0e15a9308056fd924679154a222327c
«Anastasia <nastya-ryzhik-166@yandex.ru>
2022-07-28 20:06:14 +0300

    created 2 string massives

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿/* Задача: Написать программу, которая из имеющегося массива строк 
+формирует массив из строк, длина которых меньше либо равна 3 символа. 
+Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма */
+
+/* Примеры:
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+["Russia", "Denmark", "Kazan"] -> [] */
+
+void Print(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] array1 = new string[] { "hello", "2", ";-)" };
+string[] array2 = new string[array1.Length];
+
+Print(array1);
+Print(array2);
